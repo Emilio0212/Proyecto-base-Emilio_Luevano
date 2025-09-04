1078407e82121894a96048ea95d57e85c61323d6
Emilio0212 <194541-4@iberoleon.edu.mx>
2025-09-04 13:51:36 -0600

    Version 1

---
+++ avanzadas.py
@@ -0,0 +1,14 @@
+#Funcion Potencia y raiz cuadrada
+
+leerNumeros = lambda: (int(input("Dame el primer numero: ")), int(input("Dame el segundo numero: ")))
+
+class Potencia:
+    def __init__(self, base, exponente):
+        self.base = base
+        self.exponente = exponente
+
+    def calcularPotencia(self):
+        return self.base ** self.exponente
+    
+
+    
